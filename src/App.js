093cea8e422387d17397f6f333f4d3e2d6d008d3
bweegee <bw33g33@gmail.com>
2019-03-12 21:37:29 -0700

    EditCards and StudyCards components added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EditCards from './EditCards';
+import StudyCards from './StudyCards';
 
 class App extends Component {
+	state = {
+		cardNum: 0,
+		showAnswer: false,
+		cards: [
+			{id: 1, front: "Question", back: "Answer", learning: false, },
+			{id: 2, front: "let", back: "let is block scoped.", learning: true, },
+			{id: 3, front: "Arrow function", back: "=> ", learning: true, },
+		]
+	}
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+		return (
+			<div>
+				<EditCards 
+					// cards={this.state.cards} 
+				/>
+				<StudyCards
+					// cards={this.state.cards}
+				/>
+			</div>
+		)
   }
 }
 
---
+++ src/EditCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class EditCards extends React.Component {
+	render() {
+		return (
+			<h1>Edit Cards</h1>
+		)
+	}
+}
+
+
+export default EditCards;
